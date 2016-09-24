9b7071416f550af84dd0e2970015523ffc2fb9c7
DictumMortuum <theio.vrefos@gmail.com>
2016-09-24 04:19:40 +0300

    minimist

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
+"use strict";
+
 const fs = require('fs');
 const request = require('request');
-const conf = {
-  api: 'http://transifex.com/api/2',
-  path: 'tmp',
-  ttl: 10000 //ms
-};
-
-(function(arg) {
-  var info = {};
-  var req;
-
-  if(arg.length == 1) {
-    info.url = `${conf.api}/language/${arg[0]}`,
-    info.file = `${conf.path}/${arg[0]}`
-    info.csv = false;
-  } else {
-    info.url = `${conf.api}/languages`,
-    info.file = `${conf.path}/languages`
-    info.csv = true;
+const csv = require('json2csv-stream');
+const conf = require('minimist')(process.argv.slice(2), {
+  default: {
+    api: 'http://transifex.com/api/2',
+    path: '/tmp',
+    ttl: 10000 //ms
   }
+});
 
-  fs.stat(info.file, (err, stats) => {
-    // If the cached item has expired hit the API again and re-cache.
-    if(err || new Date - stats.birthtime > conf.ttl) {
-      req = request(info.url);
-      req.pipe(fs.createWriteStream(info.file));
-    } else {
-      req = fs.createReadStream(info.file);
-    }
+var info = {};
 
-    if(info.csv) {
-      const csv = require('json2csv-stream');
-      req.pipe(new csv()).pipe(fs.createWriteStream('out.csv'));
-    }
-  });
-})(process.argv.slice(2));
+if(conf._.length == 1) {
+  info.url = `${conf.api}/language/${conf._[0]}`,
+  info.file = `${conf.path}/${conf._[0]}`
+} else {
+  info.url = `${conf.api}/languages`,
+  info.file = `${conf.path}/languages`
+}
+
+fs.stat(info.file, (err, stats) => {
+  var req;
+  // If the cached item has expired hit the API again and re-cache.
+  if(err || new Date - stats.birthtime > conf.ttl) {
+    req = request(info.url);
+    req.pipe(fs.createWriteStream(info.file));
+  } else {
+    req = fs.createReadStream(info.file);
+  }
+  // .csv generation occurs when no arguments are provided.
+  if(conf._.length == 0) {
+
+    req.pipe(new csv()).pipe(fs.createWriteStream('out.csv'));
+  }
+  req.pipe(process.stdout);
+});
